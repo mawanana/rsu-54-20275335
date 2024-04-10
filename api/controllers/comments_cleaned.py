e17acabc8d79cb78f5b05fdad28488facdebc6e2
eranda66 <eranda.i@ceylonsolutions.com>
2024-04-10 18:26:48 +0530

    main-setup: venv, aiirflow and mysql connection setup

--- api/controllers/comments_cleaned.py
+++
@@ -1,5 +0,0 @@
-from model.comments_cleaned_data import get_data_comments_cleaned_data
-
-def process_api_request_comments_cleaned_data():
-    data = get_data_comments_cleaned_data()
-    return data
